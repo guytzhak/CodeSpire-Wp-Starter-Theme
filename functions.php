24a6a43e806c07ef82047e6f1c2b23722f137c8a
Guy Ytzhak <gytzhak.seo@gmail.com>
2017-01-05 00:40:22 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -23,7 +23,15 @@ function cs_theme_setup(){
 }
 
 function cs_after_setup_theme() {
-    add_theme_support( 'html5', array( 'search-form' ) );
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'html5', array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ) );
 }
 add_action( 'after_setup_theme', 'cs_after_setup_theme' );
 
