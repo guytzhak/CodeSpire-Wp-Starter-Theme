dc909a7f0c81f1d7129cdbbc469ff7ab95ddae7b
Guy Ytzhak <gytzhak.seo@gmail.com>
2017-01-21 16:29:52 +0200

    Update core_func.php

--- inc/core_func.php
+++ inc/core_func.php
@@ -10,11 +10,21 @@ add_filter( 'embed_oembed_html', 'cs_embed_html', 10, 3 );
 add_filter( 'video_embed_html', 'cs_embed_html' );
 
 //Remove Unnecessary Code
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'wp_generator');
-remove_action('wp_head', 'start_post_rel_link');
-remove_action('wp_head', 'index_rel_link');
-remove_action('wp_head', 'adjacent_posts_rel_link');
+function cs_remove_unnecessary_code_setup () {
+    remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wp_shortlink_wp_head');
+    remove_action('wp_head', 'start_post_rel_link');
+    remove_action('wp_head', 'index_rel_link');
+    remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);
+    add_filter('the_generator', '__return_false');
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action('wp_head', 'feed_links_extra', 3);
+    remove_action('set_comment_cookies', 'wp_set_comment_cookies');
+    add_filter('jpeg_quality', create_function( '', 'return 85;' ) );
+}
+add_action('after_setup_theme', 'cs_remove_unnecessary_code_setup');
 
 ?>
