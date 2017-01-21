0942c6dc1d0cbb0d6eef1614ea2c1c38b09b2ecb
Guy Ytzhak <gytzhak.seo@gmail.com>
2017-01-21 16:21:57 +0200

    Update core_func.php

--- inc/core_func.php
+++ inc/core_func.php
@@ -2,11 +2,19 @@
 
 // Core Functions
 
-function alx_embed_html( $html ) {
+function cs_embed_html( $html ) {
     return '<div class="embed-responsive embed-responsive-16by9">' . $html . '</div>';
 }
  
-add_filter( 'embed_oembed_html', 'alx_embed_html', 10, 3 );
-add_filter( 'video_embed_html', 'alx_embed_html' ); // Jetpack
+add_filter( 'embed_oembed_html', 'cs_embed_html', 10, 3 );
+add_filter( 'video_embed_html', 'cs_embed_html' );
+
+//Remove Unnecessary Code
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'start_post_rel_link');
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'adjacent_posts_rel_link');
 
 ?>
